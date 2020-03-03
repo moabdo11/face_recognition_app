91d5a85edc5bce3e302dedb8085dfd17876f4960
Mo Abdo <moabdo1212@gmail.com>
2020-03-03 13:17:00 -0500

    added face recognition API with Clarifai, updated LinkForm to accept image as input value and then display the image after a button event is triggered

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import 'tachyons';
+
+const app = new Clarifai.App({
+ apiKey: '1068cfeebd7b4bdb8b5ebfd2c81f7196'
+});
 
 const particlesOptions = {
   particles: {
@@ -20,6 +26,33 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+onInputChange = (event) => {
+  this.setState({input: event.target.value});
+}
+
+onButtonSubmit = () => {
+  this.setState({imageUrl: this.state.input});
+  app.models.predict(
+    Clarifai.FACE_DETECT_MODEL,
+    this.state.input)
+    .then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+}
+
   render () {
     return (
       <div className='App'>
@@ -27,8 +60,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-         {/*<FaceRecognition />*/}
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} />
+         <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
       );
   }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
 			<Tilt className="Tilt br2 shadow-3" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
- 				<div className="Tilt-inner"> 👽 </div>
+ 				<div className="Tilt-inner">  </div>
 			</Tilt>
 		</div>
 	);
